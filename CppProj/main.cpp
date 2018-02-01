f82460c59f68a892a5159f08806f962129dea555
nickolas077x <36012643+nickolas077x@users.noreply.github.com>
2018-02-01 20:57:06 +0300

    приведение к требуемуму тесту

--- CppProj/main.cpp
+++ CppProj/main.cpp
@@ -1,52 +1,43 @@
 #include <iostream>
 
 
-int myStrLen(char *string_) {
+int myStrLen(const char* string_) {
     char endOfStr = '\0';
     int sumb = 0;
+
+    if (string_ == nullptr)
+        return 0;
+
     while (*string_ != endOfStr) {
         sumb++;
         string_++;
     }
-    return sumb;
+    return sumb + 1;
 }
-
-void Test1() {
-    char instring[100];
-    char endOfStr = '\0';
-    std::cout << "test1 sring consist of 99 symb \n";
-    memset(instring, ' ', sizeof(instring));
-    instring[99] = endOfStr;
-
-    std::cout << myStrLen(instring) << std::endl;
-
-}
-
-void Test2() {
-    std::cout << "test2 sring consist of 16 symb \n";
-
-    char str[] = "hello hi bonguar";
-    std::cout << myStrLen(str) << std::endl;
-
-}
-
-void Test3() {
-    std::cout << "test3 empty sring \n";
-    char str1[] = "";
-    std::cout << myStrLen(str1) << std::endl;
-
-}
-
-
 
 
 int main() {
-    Test1();
-    Test2();
-    Test3();
+
+    int res;
+
+
+    if ((res = myStrLen("hello")) != 6)
+
+
+        printf("Test 1 - FAIL\n");
+
+    else
+
+        printf("Test 1 -  OK\n");
+
+
+    if ((res = myStrLen(nullptr)) != 0)
+
+        printf("Test 2 - FAIL\n");
+
+    else
+
+        printf("Test 2 - OK\n");
+
     getchar();
-
-
-    return 0;
 }
-
